37e4ae73c73cf8828052cb28caed727c567d12cc
thedevanshagrawal <thedevanshagrawal@gmail.com>
2024-08-24 19:42:49 +0530

    modified:   frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       // '/users': 'http://localhost:5000/api/v1'
-      '/users': 'https://copypastebackend.vercel.app/api/v1'
+      '/users': 'https://copy-paste-backend.onrender.com/api/v1'
     }
   },
   plugins: [react()],
